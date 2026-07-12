3aa7e6693e2c2ff486a82bd31fdbcf1f09476567
Grigori Koblitskiy <koblicky.g.a@effective-group.ru>
2026-07-12 14:15:23 +0300

    docs

--- sixseven.go
+++ sixseven.go
@@ -1,7 +1,9 @@
+// Package sixseven enables to say "Six seven"
 package sixseven
 
 import "fmt"
 
+// GuessWhat adds a mysterious wait before saying "Six seven"
 func GuessWhat() {
 	fmt.Println("Six seven")
 }
